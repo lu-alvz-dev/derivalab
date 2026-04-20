c8f09c9f03afbb912a7228f2c24bf7d5689f02f8
Luis Alvarez <johnlavz34@gmail.com>
2026-04-19 19:00:24 -0500

    fix(mathUtils): normalize implicit multiplication for trig expressions in mathjs validation

--- server/src/utils/mathUtils.js
+++ server/src/utils/mathUtils.js
@@ -1,11 +1,22 @@
 function normalizeExpression(expr) {
   if (!expr) return "";
 
-  return expr
-    .replace(/\s+/g, "")
-    .replace(/²/g, "^2")
-    .replace(/³/g, "^3")
-    .replace(/–/g, "-");
+  return (
+    expr
+      .replace(/\s+/g, "")
+      .replace(/²/g, "^2")
+      .replace(/³/g, "^3")
+      .replace(/–/g, "-")
+
+      // Insert * between number and variable: 2x -> 2*x
+      .replace(/(\d)(x)/g, "$1*$2")
+
+      // Insert * between variable and trig function: xsin -> x*sin
+      .replace(/(x)(sin|cos|tan)/g, "$1*$2")
+
+      // Insert * between number and trig function: 2sin -> 2*sin
+      .replace(/(\d)(sin|cos|tan)/g, "$1*$2")
+  );
 }
 
 module.exports = {
