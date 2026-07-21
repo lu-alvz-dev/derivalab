e46d866757eae0f4d4c02fd8cbd0b9a0bd67a7d3
Luis Alvarez <johnlavz34@gmail.com>
2026-07-21 18:50:20 -0500

    (feat): add demo database

---
+++ server/database/demo-users.sql
@@ -0,0 +1,47 @@
+/*
+Schema for Guest & Visitor Demo (Unregistered Access)
+*/
+
+INSERT INTO users
+(
+    email,
+    password,
+    role
+)
+VALUES
+(
+    'demo.teacher@derivalab.com',
+
+    '$2b$10$KSqDVjmj4aNtyp40aHXHd.0q1sJs1TqqeAbImRUoRZDeAR3fdsyv2',
+
+    'teacher'
+)
+
+ON CONFLICT (email)
+DO NOTHING;
+
+
+
+INSERT INTO users
+(
+    email,
+    password,
+    role,
+    teacher_id
+)
+SELECT
+
+'demo.student@derivalab.com',
+
+'$2b$10$KSqDVjmj4aNtyp40aHXHd.0q1sJs1TqqeAbImRUoRZDeAR3fdsyv2',
+
+'student',
+
+id
+
+FROM users
+
+WHERE email='demo.teacher@derivalab.com'
+
+ON CONFLICT (email)
+DO NOTHING;
