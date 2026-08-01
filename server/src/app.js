f1789c8a4d72b46983c2fdab03b94e1ddfe3a520
Luis Alvarez <johnlavz34@gmail.com>
2026-08-01 17:00:23 -0500

    Restrict request body size to prevent server overload

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ app.use(helmet());
 
 app.use(cors());
 
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
 // Routes
 const exerciseRoutes = require("./routes/exerciseRoutes");
