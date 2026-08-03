4d2e8ab93edbc3417f3ac0b5ae1de9802da20ed0
Luis Alvarez <johnlavz34@gmail.com>
2026-08-02 21:09:49 -0500

    Prepare frontend for environment variables

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getToken = () => localStorage.getItem("token");
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use((config) => {
